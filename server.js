7c596530f014f821b9f05500c89c4fd1528a8f25
liaopingping <liaopingping@sansaas.com>
2017-11-29 17:08:38 +0800

    具名聊天

--- server.js
+++ server.js
@@ -11,9 +11,17 @@ let server = require("http").createServer(app);
 let io = require("socket.io")(server);
 
 io.on("connection",function (socket) { //connection和客户端的connect不一样
+	let username;
 	console.log("客户端已连接");
 	socket.on("message",(msg)=>{
-		io.emit("message",msg);//接收到客户端消息之后广播
+		if(username){
+			io.emit("message",{username,content:msg,createAt:new Date().toLocaleString()});//接收到客户端消息之后广播
+			return;
+		}
+		username = msg;
+		io.emit("message",{username:'系统',content:`欢迎${username}来到聊天室`,createAt:new Date().toLocaleString()});//接收到客户端消息之后广播
+
+
 	});
 });
 server.listen(8080); //app.listen()等同于require("http").createServer(app).listen(8080),因为socket.io用到server就分开写
