12f47309c3d7292ed4ebd22bcac1f649ca6ebfa6
liaopingping <liaopingping@sansaas.com>
2017-11-29 17:42:33 +0800

    私聊功能

--- server.js
+++ server.js
@@ -9,16 +9,25 @@ app.get("/",(req,res)=> {
 
 let server = require("http").createServer(app);
 let io = require("socket.io")(server);
-
+let sockets = {};
 io.on("connection",function (socket) { //connection和客户端的connect不一样
 	let username;
 	console.log("客户端已连接");
 	socket.on("message",(msg)=>{
 		if(username){
+			let reg = /@([^\s]+) (\S+)/;
+			let result = msg.match(reg);
+			if(result){ //是私聊
+				let toUser = result[1];
+				let content = result[2];
+				sockets[toUser].send({username,content,createAt:new Date().toLocaleString()});
+				return
+			}
 			io.emit("message",{username,content:msg,createAt:new Date().toLocaleString()});//接收到客户端消息之后广播
 			return;
 		}
 		username = msg;
+		sockets[username] = socket; //用户名和socket要做映射
 		io.emit("message",{username:'系统',content:`欢迎${username}来到聊天室`,createAt:new Date().toLocaleString()});//接收到客户端消息之后广播
 
 
